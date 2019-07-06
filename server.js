72ef37c05e2bf9e40ac0c2bff3c5068933bf8d04
Eilaf-Alg <eilaf.algebail@gmail.com>
2019-07-06 15:24:14 +0400

    😃

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded( {extended: false}));
 
 /* GET home page. */
 app.get('/', (req,res) => res.json({
-	msg: "Hello! Amigo"
+	msg: "Hello! Amigo !"
 }));
 
 /* post to Db */
